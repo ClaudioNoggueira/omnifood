0f1efa1c4a6fdfd153116ec6ffcd1abf82368053
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-11 11:41:31 -0300

    Smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 // MOBILE NAVIGATION BUTTON CLICK EVENT
-const buttonEl = document.querySelector('.mobile-nav-button');
+const buttonEl = document.querySelector(".mobile-nav-button");
 
-buttonEl.addEventListener('click', () => {
-  const headerEl = document.querySelector('header');
-  headerEl.classList.toggle('nav-open');
+buttonEl.addEventListener("click", () => {
+  const headerEl = document.querySelector("header");
+  headerEl.classList.toggle("nav-open");
 });
 
 // COPYRIGHT YEAR
-const yearEl = document.querySelector('.copyrightYear');
+const yearEl = document.querySelector(".copyrightYear");
 
 yearEl.textContent = new Date().getFullYear();
+
+// SMOOTH SCROLLING ANIMATION
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      if (link.classList.contains("main-nav-link"))
+        document.querySelector("header").classList.remove("nav-open");
+    }
+  });
+});
